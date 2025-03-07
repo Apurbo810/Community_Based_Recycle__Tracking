9bcf7ef0b2649e672e5681dd81f715cfbe9e95f3
Apurbo810 <114826531+Apurbo810@users.noreply.github.com>
2025-03-07 13:34:49 +0600

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,16 +6,21 @@ import { AppService } from './app.service';
 import { RecyclerModule } from './recycler/recycler.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const DATABASE_URL="postgresql://test_i9q3_user:027lSHZYJv8wk5dDtQ1Mg0CMPtUsjyqn@dpg-cv59t45ds78s739bjf0g-a/test_i9q3"
+const NODE_ENV="production"
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Load environment variables
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, // Use Render's database URL
+      url:DATABASE_URL, // Use Render's database URL
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // Set to false in production
-      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Required for Render DB
+      ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Required for Render DB
     }),
     RecyclerModule,
     UserModule,
